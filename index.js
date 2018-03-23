09c588afa8c4cf25fe815c1d6ed97e1c22ab210e
Ian Canino <ian.canino@gmail.com>
2018-03-23 00:36:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var testString = string
   if (string.toLowerCase === testString) {
-    var response = "I can\'t hear you"
+    var response = 'I can\'t hear you'
     return response
   } else 
   if (string.toUpperCase === testString) {
-    return "YES INDEED!"
+    return 'YES INDEED!'
   } else
   if (string === "I love you, Grandma.") {
     return "I love you, too."
