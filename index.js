8342b3f317c72ea48edf4a6d7eeff9a4c31ec135
Ian Canino <ian.canino@gmail.com>
2018-03-23 00:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(shout(string))
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  var testString = string
+  if (string.toLowerCase === testString) {
+    return "I can't hear you!"
+  } else 
+  if (string.toUpperCase === testString) {
+    return "YES INDEED!"
+  } else
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
